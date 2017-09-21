044792a58aaa36368b180fca29307d751e957d3e
jdalton467 <jamesdalton463@gmail.com>
2017-09-21 00:46:19 -0400

    got more done more bugs for tmrw

---
+++ public/assets/javascript/index.js
@@ -0,0 +1,140 @@
+$(document).ready(function() {
+$('.dropdown-toggle').dropdown();
+
+
+var source;
+var sourceName;
+var queryURL;
+var newsTitle;
+var newsImg;
+var newsDesc;
+var newsUrl;
+var start = 0;
+
+
+$.ajax({
+    url: "https://newsapi.org/v1/sources?language=en",
+    method: "GET"
+}).done(function(response) {
+    // console.log(response);
+    // console.log(response.sources.length);
+    var length = response.sources.length;
+    // console.log(length);
+    // console.log(response.sources);
+
+    for (var i = 0, arr = []; i < 10; i++) {
+        var newNum = Math.floor(Math.random() * 60);
+        arr.push(newNum);
+    }
+    console.log("arr: " + arr);
+    for (var i = 0, sourceIdArr = [], sourceNameArr = []; i < 10; i++) {
+        sourceIdArr.push(response.sources[arr[i]].id);
+        sourceNameArr.push(response.sources[arr[i]].name);
+    }
+    // console.log("sourceIdArr: " + sourceIdArr);
+    // console.log("sourceNameArr: " + sourceNameArr);
+    for (var i = 0; i < 10; i++) {
+
+        source = sourceIdArr[i];
+        sourceName = sourceNameArr[i];
+        console.log(sourceName);
+        queryURL = "https://newsapi.org/v1/articles?source=" + source + "&apiKey=5d9f7c67d4384f35bd73aa91efca8a73";
+        // console.log(source);
+        getNews(sourceName);
+      
+        // console.log(title)
+    }
+    // displayNews();
+
+});
+
+
+
+
+
+
+
+
+
+console.log(title);
+
+function getNews(sourceName) {
+    var title, desc, img, url;
+   
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).done(function(response) {
+    	source = response.source;
+    	// console.log(response.articles[1]);
+
+        title = response.articles[1].title;
+        desc = response.articles[1].description;
+        img = response.articles[1].urlToImage;
+        url = response.articles[1].url;
+    }).done(function() {
+
+    	name = sourceName;
+        newsImg = img;
+        newsTitle = title;
+        newsDesc = desc;
+        newsUrl = url;
+       
+        
+
+
+        var newDiv = $('<div/>', {
+            class: 'carousel-item'
+        });
+        var newCard = $('<div/>', {
+            class: 'card'
+        });
+        var cardHead = $('<div/>', {
+            class: 'card-header'
+        });
+        var headline = $('<h4/>', {
+            text: name + ": " + newsTitle
+        });
+        var newImg = $('<img/>', {
+            class: 'card-img-top',
+            src: newsImg,
+            alt: 'Card image cap'
+        });
+        var a = $("<a/>", {
+            href: newsUrl
+        });
+        var cardDiv = $('<div/>', {
+            class: 'card-block'
+        });
+        a.append(newImg);
+        cardDiv.append('<p>' + newsDesc + '</p>');
+        cardHead.append(headline);
+        newCard.append(cardHead);
+        newCard.append(a);
+        newCard.append(cardDiv);
+        newDiv.append(newCard);
+       
+      
+        if(start == 0){
+        newDiv.addClass('active');
+       }
+       start++;
+       console.log("start: " + start);
+
+
+        $('.carousel-inner').append(newDiv);
+
+    });
+
+
+};
+
+
+
+
+
+});
+
+
+
+
